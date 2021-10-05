871cba4456d902002c0a9baa5ac5cf5251655d31
yasinahmad12 <yasin.inf18@student.unu-jogja.ac.id>
2021-10-05 13:05:34 +0700

    belajar coding

---
+++ 1-2/pertemuan2.py
@@ -0,0 +1,31 @@
+#string
+a= "python pemula"
+print(type(a))
+#Menyatakan karakter/kalimat bisa berupa huruf angka, dll diapit tanda " atau '
+
+
+#  integer
+print (20)
+#Menyatakan bilangan bulat
+
+#float
+print (3.14)
+#menyatakan bilangan yang mempunyai koma
+
+#list
+angka = [1,2,3,4,5]
+print (angka)
+print ('satu', 'dua', 'tiga')
+
+#tuple
+print((1,2,3,4,5))
+print(("satu", "dua", "tiga"))
+
+#dictionary
+biodata = {
+ "nama"     : "muhammad yasin",
+ "nim"      : "181111103",
+ "jurusan"  : "informatika"
+}
+print (biodata)
+print(type (biodata))
